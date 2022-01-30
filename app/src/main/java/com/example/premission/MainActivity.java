c8b9ca34f99fbb7bbf7bb3e3f50e120a659940fc
RemusLupin13579 <brhvangui12@gmail.com>
2022-01-30 16:01:42 +0200

    permission start phase

--- app/src/main/java/com/example/premission/MainActivity.java
+++ app/src/main/java/com/example/premission/MainActivity.java
@@ -32,7 +32,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     PermissionUtil permissionUtil;
     Button btnRead, btnWrite, btnCamera, btnAll;
-
+//int
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
